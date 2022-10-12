ad18d3cbb2ff8c01181d6d855bff42bd645c77a2
Lucas <lucaschmain@gmail.com>
2022-10-11 23:28:21 -0300

    Error Avaliator

--- src/models/connection.js
+++ src/models/connection.js
@@ -3,13 +3,9 @@ require('dotenv').config();
 
 const connection = mysql.createPool({
   host: process.env.MYSQL_HOST,
-  port: process.env.MYSQL_PORT,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  waitForConnections: process.env.MYSQL_WAIT_FOR_CONNECTIONS,
-  connectionLimit: process.env.MYSQL_CONNECTION_LIMIT,
-  queueLimit: process.env.MYSQL_QUEUE_LIMIT,
+  multipleStatements: true,
 });
 
 module.exports = connection;
